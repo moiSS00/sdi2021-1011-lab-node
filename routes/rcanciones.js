50165b9c261261a1b26980761b402915dcbaf66d
uo270824 <uo270824@uniovi.es>
2021-03-25 18:25:56 +0100

    SDI-2021-1011-7.3-División de las rutas en módulos, peticiones GET y parámetros

---
+++ routes/rcanciones.js
@@ -0,0 +1,13 @@
+module.exports = function(app) {
+    app.get("/canciones", function(req, res) {
+        let respuesta = "";
+
+        if(req.query.nombre != null)
+            respuesta += 'Nombre: ' + req.query.nombre + '<br>';
+
+        if(typeof (req.query.autor) != "undefined")
+            respuesta += 'Autor: '+ req.query.autor;
+
+        res.send(respuesta);
+    });
+};
---
+++ routes/rusuarios.js
@@ -0,0 +1,16 @@
+module.exports = function(app) {
+    app.get("/usuarios", function(req, res) {
+        res.send("ver usuarios");
+    });
+
+    app.get('/canciones/:id', function(req, res) {
+        let respuesta = 'id: ' + req.params.id;
+        res.send(respuesta);
+    });
+
+    app.get('/canciones/:genero/:id', function(req, res) {
+        let respuesta = 'id: ' + req.params.id + '<br>'
+            + 'Género: ' + req.params.genero;
+        res.send(respuesta);
+    });
+};
